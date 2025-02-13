17794ede4c007eb2bedf52a995a473bf0edc16e0
Markubys <83160020+Markubys@users.noreply.github.com>
2025-02-13 08:18:34 +0000

    Tutorial2

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+from django.contrib import admin 
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
